0f08f1bc9c5df3a935a02b78536c20f87337ff9f
Jenna Tan <9787333+jenna215@users.noreply.github.com>
2023-09-06 13:23:02 -0400

    all tests passed

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,24 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.isEmpty()) {
+            System.out.print("No Results");
+        }
+        for (HashMap<String, String> jobs : someJobs) {
+            System.out.println("\n*****");
+            for (Map.Entry<String, String> job : jobs.entrySet()) {
+                System.out.println(job.getKey() + ": " + job.getValue());
+            }
+            System.out.println("*****");
 
-        System.out.println("printJobs is not implemented yet");
+//            System.out.println("printJobs is not implemented yet");
+        }
     }
 }
